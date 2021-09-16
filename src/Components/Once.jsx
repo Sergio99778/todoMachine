71c64fcd6b24c2744c188a2387b797071033f52b
Sergio99778 <sergio4brandon@gmail.com>
2021-09-16 01:26:01 -0500

    Update Once component.

--- src/Components/Once.jsx
+++ src/Components/Once.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './styles/Once.css';
-import checkmarkImage from '../assets/white-checkmark.png';
 
 function Once() {
   return (
-    <button type="checkbox" className="Once">
-      <img className="Once__icon" src={checkmarkImage} alt="Check" />
-    </button>
+    <>
+      <input type="checkbox" id="check" />
+      <label for="check">
+        <div class="check-icon"></div>
+      </label>
+    </>
   );
 }
 
